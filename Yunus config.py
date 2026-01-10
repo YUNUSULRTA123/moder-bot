0e00e8343252b42084534f10cc873451a3ac36ac
Yunus Azimli <afsananamazova01@gmail.com>
2026-01-10 19:03:23 +0400

    Create Yunus config.py

---
+++ Yunus config.py
@@ -0,0 +1,18 @@
+# config.py
+
+BOT_TOKEN = "8401783475:AAEc0_jBqOQ5JiKpbRgQBbFLTMYPphYSUyA"
+
+ANTI_AD_ENABLED = True
+
+STOP_WORDS = {
+    "заработок",
+    "крипта",
+    "казино",
+    "подпишись"
+}
+
+SPAM_LIMIT = 3          # одинаковых сообщений
+SPAM_WINDOW = 60        # секунд
+MUTE_TIME = 600         # секунд
+MAX_WARNINGS = 3
+ADMIN_IDS = []  # Замените на реальные ID администраторов
